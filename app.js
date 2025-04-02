95a191ef0a29bb84c021158b6a711435521478f2
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-01 20:45:08 -0600

    2nd solotion coded but output is not correct

--- app.js
+++ app.js
@@ -9,6 +9,67 @@ function incrementCounter() {
   }
 }
 
-incrementCounter();
+/* run the console.log for see the output */
+// incrementCounter();
 
-// maximum is 8370 
+// maximum is 8370
+
+///////////////-----------------------------
+/*Step One: write the recursive function.
+ *
+ * Here, we create a function that calculates
+ * the factorial of a number, n. A factorial
+ * is the product of all positive integers
+ * less than or equal to the number, n.
+ */
+function factorial(n) {
+  if (n === 0) return 1; // The base case, to stop recursion
+  return n * factorial(n - 1); // The recursive call
+}
+// console.log(factorial(5));
+
+/**
+ * If we were to call the above with a number as
+ * high as, say, 50,000, it would result in a stack
+ * overflow.
+ */
+
+/////////------------------------------------>
+// part 2
+
+/*
+ *Write a recursive function that completely flattens an array of nested arrays,
+ *regardless of how deeply nested the arrays are.
+ *Once your recursive function is complete, trampoline it.
+ */
+
+// first need same deep nested array
+// create flattenArr function
+// declare empty arr for output
+// create flatten function inside flattenArr function
+// iterate over the given array and check if the iterated array still has array inside it,
+// if yes recursion and repeat the process
+// otherwise push the item of iterated array into output array
+// call the flatten function for recursion  until all all nested arrays are out
+// return the output array
+
+const deepNestedArr = [[[1, [2, [3, 4], 5]], [6, [[7, 8]], 9], [10]]];
+
+function flattenArr(arr) {
+  let output = [];
+
+  function flatten(subArr) {
+    for (let item of subArr) {
+      if (item !== Array.isArray(item)) {
+        output.push(item);
+      } else {
+        flatten(item);
+      }
+    }
+  }
+  flatten(arr);
+  return output;
+}
+
+/* run the console.log for see the output */
+console.log(flattenArr(deepNestedArr));
